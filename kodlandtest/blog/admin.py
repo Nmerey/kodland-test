5cffc17ba49a060bcfa7750d6d84d90b5ff645fc
Nmerey <merey.nurlan@gmail.com>
2020-08-01 02:56:27 +0600

    Add homepage

--- kodlandtest/blog/admin.py
+++ kodlandtest/blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Post 
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','created_on','slug')
+    search_fields = ['title']
+
+admin.site.register(Post, PostAdmin)
--- kodlandtest/blog/models.py
+++ kodlandtest/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    title 		= models.CharField(max_length=200, unique=True)
+    updated_on 	= models.DateTimeField(auto_now= True)
+    slug		= models.SlugField(max_length=200,unique=True)
+    content 	= models.TextField()
+    created_on 	= models.DateTimeField(auto_now_add=True)
+    image 		= models.ImageField(upload_to='post')
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
