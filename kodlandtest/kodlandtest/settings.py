588ac147669283604570103f1da2e0c104f6cabc
Nmerey <merey.nurlan@gmail.com>
2020-08-07 03:14:16 +0600

    Fix database settings

--- kodlandtest/kodlandtest/settings.py
+++ kodlandtest/kodlandtest/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'hd7z5j+3x(h-zgghd@q@7ud@!#6r$%!bujb-jw_4nj_gp-#&l$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
